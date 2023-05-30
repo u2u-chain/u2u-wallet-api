85995c594fe92d5f31e772efec710343af9f373b
Edward Hedenberg <edward@uniultra.xyz>
2023-05-30 23:02:05 +0700

    :sparkles: implement data validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpException, HttpStatus, ValidationError, ValidationPipe } from "@nestjs/common";
 
 const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors: ValidationError[]) => {
+      const firstError = errors[0].constraints;
+      return new HttpException(firstError[Object.keys(firstError)[0]], HttpStatus.BAD_REQUEST);
+    }
+  }));
   await app.listen(port);
 }
 bootstrap().then(() => {
--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,7 +1,16 @@
+import { IsEmail, IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserInput {
+  @IsEmail({}, {always: true, message: 'Invalid email'})
   email: string;
+
+  @IsNotEmpty({always: true, message: 'Name cannot be blank'})
+  @Matches(/^([^0-9!@#$%^&*()_+=[\]{}|\\,./?<>;:"“”‘’·„‚«»‹›]+ [^0-9!@#$%^&*()_+=[\]{}|\\,./?<>;:"“”‘’·„‚«»‹›]+)+$/i, {message: 'Invalid name'})
   fullName: string;
+
+  @IsNotEmpty({always: true, message: 'Password cannot be blank'})
+  @MinLength(8, {always: true, message: 'Password is too short'})
+  @MaxLength(64, {always: true, message: 'Password is too long'})
   password: string;
 }
 
