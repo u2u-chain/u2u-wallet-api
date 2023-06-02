088606d3349ea7ad1f7c8ec76f977a1b55d24ace
Edward Hedenberg <edward@uniultra.xyz>
2023-06-02 16:54:08 +0700

    :bug: disable cors temporarily

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:5173',
-      'http://localhost:3000',
+      '*',
     ],
     methods: ["GET", "POST"],
     credentials: true,
